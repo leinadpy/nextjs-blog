d6dde3823fe2063768402e67d632309cc7ace8ee
Daniel Ojeda <leinadpy@gmail.com>
2021-05-07 08:02:49 -0400

    first commit

---
+++ components/Profile.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Profile = () => (
+  <Image
+    src="/images/profile.jpg"
+    height={144}
+    width={144}
+    alt="Daniel Ojeda"
+  />
+);
+
+export default Profile;
